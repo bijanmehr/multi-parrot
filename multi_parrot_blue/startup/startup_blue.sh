3a064cd4dadcbdb415b801882de9f5860bb2d20c
bijan <bijanmehralizadeh@gmail.com>
2020-11-04 02:05:21 +0330

    update startup script

--- multi_parrot_blue/startup/startup_blue.sh
+++ multi_parrot_blue/startup/startup_blue.sh
@@ -2,4 +2,4 @@
 
 source $(rospack find multi_parrot_blue)/startup/ROS_param.sh
 
-gnome-terminal -e "bash -c \"roslaunch multi_parrot_blue main.launch; exec bash\""
+roslaunch multi_parrot_blue main.launch
--- multi_parrot_red/startup/startup_red.sh
+++ multi_parrot_red/startup/startup_red.sh
@@ -2,4 +2,4 @@
 
 source $(rospack find multi_parrot_red)/startup/ROS_param.sh
 
-gnome-terminal -e "bash -c \"roslaunch multi_parrot_red main.launch; exec bash\""
+roslaunch multi_parrot_red main.launch
