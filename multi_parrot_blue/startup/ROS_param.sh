857a40bf9674b3f5a6a496594f9213ed9ff1b381
bijanmehr <bijanmehralizadeh@gmail.com>
2020-11-09 14:53:04 +0330

    update network parameter

--- multi_parrot_blue/startup/ROS_param.sh
+++ multi_parrot_blue/startup/ROS_param.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export ROS_IP=192.168.1.102
-export ROS_MASTER_URI=http://192.168.1.173:11311
+export ROS_IP=192.168.0.105
+export ROS_MASTER_URI=http://192.168.0.104:11311
--- multi_parrot_blue/startup/startup_blue.sh
+++ multi_parrot_blue/startup/startup_blue.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# sleep 10
 source $(rospack find multi_parrot_blue)/startup/ROS_param.sh
 
-roslaunch multi_parrot_blue main.launch
+roslaunch multi_parrot_blue blue_parrot.launch
