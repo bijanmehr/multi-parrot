3793fe21a4d621b684098f275f23642063252515
SHB <shahabnikkhoo@gmail.com>
2020-11-18 11:43:19 +0330

    update startup files

--- multi_parrot_web/startup/startup.sh
+++ multi_parrot_web/startup/startup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+sleep 5
+
 source /home/$(whoami)/Desktop/multi-parrot/multi_parrot_web/startup/ROS_param.sh
 
 gnome-terminal -e "bash -c \"python3 /home/$(whoami)/Desktop/multi-parrot/multi_parrot_web/manage.py runserver 0.0.0.0:8000 --noreload ; exec bash\""
 
-sleep 2
+sleep 1
 
-roscore
+roslaunch main.launch
