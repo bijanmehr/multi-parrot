a1b394165216fd90829fec516b3ed60a3c6c6329
SHB <shahabnikkhoo@gmail.com>
2020-11-24 17:48:48 +0330

    update autorun scripts

--- multi_parrot_web/startup/startup.sh
+++ multi_parrot_web/startup/startup.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
-sleep 5
+# sleep 5
 
+# gnome-terminal -e  "roslaunch /home/$(whoami)/Desktop/multi-parrot/multi_parrot_web/startup/main.launch"
+
+# sleep 5
+
+# gnome-terminal -e "python3 /home/$(whoami)/Desktop/multi-parrot/multi_parrot_web/manage.py runserver 0.0.0.0:8000 --noreload "
+
+
+# exec $SHELL
+
+
+source /opt/ros/kinetic/setup.bash
 source /home/$(whoami)/Desktop/multi-parrot/multi_parrot_web/startup/ROS_param.sh
+# source /home/cabinet/catkin_ws/devel/setup.bash
 
-gnome-terminal -e "bash -c \"python3 /home/$(whoami)/Desktop/multi-parrot/multi_parrot_web/manage.py runserver 0.0.0.0:8000 --noreload ; exec bash\""
 
-sleep 1
 
-roslaunch main.launch
+gnome-terminal -e "python3 /home/$(whoami)/Desktop/multi-parrot/multi_parrot_web/manage.py runserver 0.0.0.0:8000 --noreload "
+
+sleep 2
+
+roslaunch /home/$(whoami)/Desktop/multi-parrot/multi_parrot_web/startup/main.launch
+
